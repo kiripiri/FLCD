f4f2af058c00aa26521c9bb92363d43d5d584779
kiripiri <loredana.purcel999@gmail.com>
2020-10-14 09:29:27 +0300

    lab2

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] argv) {
+        String[] months = {"define","number","number1","=","10",";"};
+        Tree tree = new Tree();
+        for (int i = 0; i < months.length; i++)
+            tree.insert(months[i]);
+        tree.inorderTraversal();
+        System.out.println();
+    }
+}
